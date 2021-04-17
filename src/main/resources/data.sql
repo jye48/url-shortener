1505400b5dee7b780d281bbf8c52744613d15c9e
Jerry Ye <jerry33@michaels.com>
2021-04-17 01:42:41 -0500

    Create application.yml from application.properties and delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS url_map;
-
-CREATE TABLE url_map
-(
-    id                INT AUTO_INCREMENT PRIMARY KEY,
-    shortened_link    VARCHAR(16)   NOT NULL,
-    original_url      VARCHAR(2048) NOT NULL,
-    created_timestamp TIMESTAMP     NOT NULL
-);
