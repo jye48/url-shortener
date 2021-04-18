116a604bf684fe4e32bd228de5418c381a0546e2
Jerry Ye <jerry33@michaels.com>
2021-04-18 01:19:36 -0500

    Add request validation to the targetUrl field

--- src/main/java/co/bulletin/urlshortener/model/CreateShortUrlRequest.java
+++ src/main/java/co/bulletin/urlshortener/model/CreateShortUrlRequest.java
@@ -1,9 +1,11 @@
 package co.bulletin.urlshortener.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @NoArgsConstructor
@@ -11,5 +13,7 @@ import lombok.NoArgsConstructor;
 public class CreateShortUrlRequest {
 
   @NotBlank
-  private String longUrl;
+  @URL
+  @Size(max = 2048)
+  private String targetUrl;
 }
