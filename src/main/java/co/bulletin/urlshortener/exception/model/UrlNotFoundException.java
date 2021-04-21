4d5cb5d79ac4bc6afb8718c31bc9d123e1ca09d5
Jerry Ye <jerry33@michaels.com>
2021-04-21 00:44:13 -0500

    Update error message

--- src/main/java/co/bulletin/urlshortener/exception/model/UrlNotFoundException.java
+++ src/main/java/co/bulletin/urlshortener/exception/model/UrlNotFoundException.java
@@ -2,7 +2,7 @@ package co.bulletin.urlshortener.exception.model;
 
 public class UrlNotFoundException extends RuntimeException {
 
-  public UrlNotFoundException(String shortUrl) {
-    super("short url " + shortUrl + " not found");
+  public UrlNotFoundException(String shortUrlId) {
+    super("short url with id " + shortUrlId + " not found");
   }
 }
