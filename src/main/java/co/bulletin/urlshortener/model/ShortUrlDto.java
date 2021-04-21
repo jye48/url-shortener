58b627dcd7e461e0861281745f0e4f45084619d0
Jerry Ye <jerry33@michaels.com>
2021-04-21 01:05:41 -0500

    Add @NoArgsConstructor and @AllArgsConstructor annotations to the class

--- src/main/java/co/bulletin/urlshortener/model/ShortUrlDto.java
+++ src/main/java/co/bulletin/urlshortener/model/ShortUrlDto.java
@@ -1,13 +1,9 @@
 package co.bulletin.urlshortener.model;
 
 import java.time.Instant;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ShortUrlDto {
 
   private String shortUrlId;
